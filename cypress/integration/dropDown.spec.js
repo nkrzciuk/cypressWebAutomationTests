489b6245e24579ec8176f2cafcfd30ca4aa84a98
Natalia Krzciuk <natalia.krzciuk@itmagination.com>
2022-04-07 13:11:53 +0200

    podpunkty g i h z zadania 4

--- cypress/integration/dropDown.spec.js
+++ cypress/integration/dropDown.spec.js
@@ -3,6 +3,7 @@ describe('DROPDOWN, CHECKBOXE(S) & RADIO BUTTON(S) - automation', function() {
     let firstDropdown = ['JAVA', 'C#', 'Python', 'SQL']
     let secondDropdown = ['Eclipse', 'Maven', 'TestNG', 'JUnit']
     let thirdDropdown = ['HTML', 'CSS', 'JavaScript', 'JQuery']
+    let radioButtonsValues = ['green', 'blue', 'yellow', 'orange', 'purple']
 
 beforeEach(() => {
     cy.visit('/Dropdown-Checkboxes-RadioButtons/index.html')
@@ -29,11 +30,23 @@ it ('Check values from second dropdown list', function(){
 })
 
 it ('Check values from third dropdown list', function(){
-    secondDropdown.forEach((option) => {
-        cy.get('[id=dropdowm-menu-2]')
+    thirdDropdown.forEach((option) => {
+        cy.get('[id=dropdowm-menu-3]')
         .select(option)
         .invoke('val')
         .should('deep.equal', option.toLowerCase())
       });
 })
+
+it ('Checkboxes 2 and 4 unchecked correctly', function(){
+    cy.get('[type="checkbox"]').check().should('be.checked')
+    cy.get('[type="checkbox"]').uncheck(['option-2', 'option-4']).should('not.be.checked')
+})
+
+it ('Radio buttons, click and check', function(){
+    radioButtonsValues.forEach((value) => {
+        cy.get('[type="radio"]').check(value).should('be.checked').and('have.value', value)
+    })
+})
+
 })
