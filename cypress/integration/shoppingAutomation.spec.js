6b7cafcaab6632f2f36005b661acd13747095730
Natalia Krzciuk <natalia.krzciuk@itmagination.com>
2022-04-15 18:33:00 +0200

    complete first part

--- cypress/integration/shoppingAutomation.spec.js
+++ cypress/integration/shoppingAutomation.spec.js
@@ -1,10 +1,11 @@
+import { func } from "assert-plus"
 import {shopping} from "../support/page_object/shoppingPage"
 
 describe('Shopping Page automation', function() {
 let testData
 
 beforeEach(() => {
-    cy.visit('http://automationteststore.com/')
+    cy.visit('https://automationteststore.com/')
 })
 this.beforeAll(() => {
     cy.fixture('testData').then(function (data) {
@@ -13,27 +14,57 @@ this.beforeAll(() => {
     })
 })
 
-function chooseCategoryAndAddItemToCart (category, items) {
+function chooseCategoryAndAddItemToCart (category, items, size) {
     cy.get(shopping.navMenu).contains(category).click()
     cy.contains(items, {force: true}).click({force: true})
+    cy.get(shopping.sortItems).select("Date New > Old")
     cy.get(shopping.productCart).first().invoke('text').then( itemText => {
         cy.get(shopping.productCart).first().click()
         cy.get(shopping.productName).find('span').should('have.text', itemText)
-        if (items == "Shoes") {
-            cy.get("[type='radio']").first().check()
-        } else {
-
-        }
-        // cy.get("[type='radio']").first().check()
+        cy.get(shopping.itemForm).find('select').select(size)
         cy.contains(shopping.addToCartButtonText).click()
         cy.contains(testData.emptyShoppingCartMsg).should('not.exist')
         cy.get('a').contains(itemText)
 })
 }
 
+function searchForTheProductAndAddItemToCart (productName) {
+    cy.get(shopping.searchInput).type(productName)
+    .type('{enter}')
+    cy.get(shopping.productCart).first().invoke('text').then( itemText => {
+        cy.get(shopping.productCart).first().click()
+        cy.get(shopping.productName).find('span').should('have.text', itemText)
+        // cy.get(shopping.itemForm).find('select').select(size)
+        cy.contains(shopping.addToCartButtonText).click()
+        cy.contains(testData.emptyShoppingCartMsg).should('not.exist')
+        cy.get('a').contains(itemText)
+})
+}
+
+function fillClientDataToCompleteOrder (name, lastname, email, address, country, region, city, postCode) {
+    cy.get(shopping.clientName).type(name)
+    cy.get(shopping.clientLastName).type(lastname)
+    cy.get(shopping.clientEmail).type(email)
+    cy.get(shopping.clientAddress).type(address)
+    cy.get(shopping.selectCountry).select(country)
+    cy.get(shopping.selectRegion).select(region)
+    cy.get(shopping.clientCity).type(city)
+    cy.get(shopping.zipCode).type(postCode)
+}
+
 it ('Add items to cart and finalize order', function(){
-    chooseCategoryAndAddItemToCart("Apparel & accessories", "Shoes");
-    chooseCategoryAndAddItemToCart("Apparel & accessories", "T-shirts");
+    chooseCategoryAndAddItemToCart("Apparel & accessories", "Shoes", "39");
+    chooseCategoryAndAddItemToCart("Apparel & accessories", "T-shirts", "Large");
+    searchForTheProductAndAddItemToCart(testData.cosmeticNameSearch);
+    cy.get(shopping.goToCheckout).click()
+    cy.get('[type="radio"]').check('guest')
+    cy.get(shopping.continueButton).click()
+    fillClientDataToCompleteOrder(testData.firstName, testData.lastName, testData.email, testData.address, 
+        testData.country, testData.region, testData.city, testData.postCode)
+    cy.get(shopping.continueButton).click()
+    cy.get(shopping.confirmOrderButton).click()
+    cy.get(shopping.orderConfirmationMsg).should('have.text', testData.successfullOrderMsg)
+
 })
 
 })
--- cypress/support/page_object/shoppingPage.js
+++ cypress/support/page_object/shoppingPage.js
@@ -2,6 +2,22 @@ const shopping = {
     navMenu: '[class="nav-pills categorymenu"]',
     productCart: '[class="prdocutname"]',
     productName: 'h1[class="productname"]',
-    addToCartButtonText: 'Add to Cart'
+    addToCartButtonText: 'Add to Cart',
+    sortItems: '[id="sort"]',
+    itemForm: 'form[id="product"]',
+    searchInput: '[id=filter_keyword]',
+    clientName: '[name="firstname"]',
+    clientLastName: '[name="lastname"]',
+    clientEmail: 'input[id=guestFrm_email]',
+    clientAddress: '[name="address_1"]',
+    clientCity: '[name="city"]',
+    selectRegion: '[name="zone_id"]',
+    zipCode: '[name="postcode"]',
+    selectCountry: '[name="country_id"]',
+    continueButton: '[title="Continue"]',
+    confirmOrderButton: '[id=checkout_btn]',
+    orderConfirmationMsg: 'span[class="maintext"]',
+    goToCheckout: '[id=cart_checkout1]'
+
 }
 export { shopping };
