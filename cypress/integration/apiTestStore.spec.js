fa758213db6204c699ec412c6049620d9dedebe5
Natalia Krzciuk <natalia.krzciuk@itmagination.com>
2022-04-22 17:07:34 +0200

    code refactor

--- cypress/integration/apiTestStore.spec.js
+++ cypress/integration/apiTestStore.spec.js
@@ -1,7 +1,7 @@
 describe('API testing STORE', function() {
     let testData;
     const currentDate = new Date()
-    const storeURL = "https://petstore.swagger.io/v2/store/"
+    const storeURL = 'https://petstore.swagger.io/v2/store/'
 
 this.beforeAll(() => {
     cy.fixture('testData').then(function (data) {
@@ -11,8 +11,15 @@ this.beforeAll(() => {
 })
 
 function addPetToTheStore (dogName, idNo) {
-    cy.request('POST', "https://petstore.swagger.io/v2/pet/" , { name: dogName, "photoUrls": [
-        "imageURL"], id: idNo}).then(
+    cy.request({
+        method: 'POST',
+        url: 'https://petstore.swagger.io/v2/pet/',
+        body: {
+            name: dogName,
+            photoUrls: ['imageURL'],
+            id: idNo
+        }
+    }).then(
         (response) => {
           expect(response.status).to.eq(200)
           expect(response.body).to.have.property('name', dogName)
@@ -22,8 +29,18 @@ function addPetToTheStore (dogName, idNo) {
 
 it ('Place an order for the pet', function(){
     addPetToTheStore(testData.dog1Name, testData.dogID)
-    cy.request('POST', storeURL + "order", { id: 1, petId: testData.dogID, quantity: testData.petQuantity,
-    shipDate: currentDate.toISOString(), status: "placed", complete: true}).then(
+    cy.request({
+        method: 'POST',
+        url: storeURL + "order",
+        body: {
+            id: 1,
+            petId: testData.dogID,
+            quantity: testData.petQuantity,
+            shipDate: currentDate.toISOString(),
+            status: "placed",
+            complete: true
+          }
+        }).then(
         (response) => {
           expect(response.status).to.eq(200)
           expect(response.body).to.have.property('id', 1)
@@ -35,9 +52,23 @@ it ('Place an order for the pet', function(){
 })
 
 it ('Find order by ID', function(){
-    cy.request('POST', storeURL + "order", { id: testData.orderID, petId: testData.dogID, quantity: testData.petQuantity,
-        shipDate: currentDate.toISOString(), status: "placed", complete: true})
-    cy.request('GET', storeURL+ "order/" + testData.orderID, {}).then( (response) => {
+    cy.request({
+        method: 'POST',
+        url: storeURL + 'order', 
+        body: {
+            id: testData.orderID,
+            petId: testData.dogID,
+            quantity: testData.petQuantity,
+            shipDate: currentDate.toISOString(),
+            status: 'placed',
+            complete: true
+          }
+        })
+    cy.request({
+        method: 'GET', 
+        url: storeURL+ 'order/' + testData.orderID, 
+        body: {}
+    }).then( (response) => {
           expect(response.status).to.eq(200)
           expect(response.body).to.have.property('id', testData.orderID)
           expect(response.body).to.have.property('status', 'placed')
@@ -46,16 +77,34 @@ it ('Find order by ID', function(){
 })
 
 it ('Delete order by ID', function(){
-    cy.request('POST', storeURL + "order", { id: testData.orderID, petId: testData.dogID, quantity: testData.petQuantity,
-        shipDate: currentDate.toISOString(), status: "placed", complete: true})
-    cy.request('DELETE', storeURL+ "order/" + testData.orderID, {}).then( (response) => {
+    cy.request({
+        method: 'POST',
+        url: storeURL + 'order',
+        body: {
+            id: testData.orderID,
+            petId: testData.dogID,
+            quantity: testData.petQuantity,
+            shipDate: currentDate.toISOString(),
+            status: 'placed',
+            complete: true
+        }
+        })
+    cy.request({
+        method: 'DELETE',
+        url: storeURL+ 'order/' + testData.orderID,
+        body: {}
+    }).then( (response) => {
           expect(response.status).to.eq(200)
           expect(response.body).to.have.property('message', testData.orderID.toString())
     })
 })
 
 it ('Return store inventory by pet status', function(){
-    cy.request('GET', storeURL+ "inventory/", {}).then( (response) => {
+    cy.request({
+        method: 'GET',
+        url: storeURL + 'inventory/',
+        body: {}
+    }).then( (response) => {
           expect(response.status).to.eq(200)
           expect(response.body).to.have.property('available')
           expect(response.body).to.have.property('pending')
